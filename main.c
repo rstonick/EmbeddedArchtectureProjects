a1dbeb7cd1c39801906aba4883eb6cd14f95fae5
rstonick <32536147+rstonick@users.noreply.github.com>
2022-09-27 20:36:51 -0700

    Simple LED control in PSoC

---
+++ main.c
@@ -0,0 +1,35 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        /* Place your application code here. */
+        if (nSW1_Read() == 0)
+            LED1_Write(1);
+        else
+            LED1_Write(0);
+    }
+}
+
+/* [] END OF FILE */
